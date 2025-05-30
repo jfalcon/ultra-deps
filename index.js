2f602da85cb3caaa24ad4032001b269120a738bb
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-05-29 21:10:08 -0500

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// this does nothing and tree shaking makes this file moot, also only production dependencies
+// should be listed as a peer dependency here. if it's a dev dependency in a MFE application
+// it does not not need to be enforced globally
+
+// also, leave this package as a CJS export
